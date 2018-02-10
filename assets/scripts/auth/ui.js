3b542b34420a3f0e41d87e89e8178de6353bb1c1
ptavarez16 <ptavarez16@yahoo.com>
2018-02-10 11:14:31 -0500

    SPA - Use toggle functions to show game board after a successful login - Sign out refreshes page for now. Need to find a way to reload modal when user signs out

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,5 +1,7 @@
 const store = require('../store')
 
+// TODO find a way to have to modal disappear after success
+
 const signUpSuccess = function (data) {
   $('#signUp-message').text('Signed Up Successfully')
   $('#signUp-message').css('background-color', 'green')
@@ -11,17 +13,20 @@ const signUpFailure = function (error) {
   $('#signUp-message').css('background-color', 'red')
   console.error(error)
 }
+// TODO find a way of reloading modal without reloading page
 
 const signInSuccess = function (data) {
   $('#signIn-message').text('Signed In Successfully')
   $('#signIn-message').css('background-color', 'green')
   console.log(data)
   store.user = data.user
+  $('div').toggle(500)
+  $('table').toggle(1000)
 }
 
 const signInFailure = function (error) {
   $('#signIn-message').text('The username/password you entered do not match. Please try again.')
-  $('#sign-in').css('background-color', 'red')
+  $('#signIn-message').css('background-color', 'red')
   console.error(error)
 }
 
@@ -39,10 +44,13 @@ const changePasswordFailure = function (error) {
 const signOutSuccess = function (data) {
   $('#signOut-message').text('Signed Out Successfully')
   $('#signOut-message').css('background-color', 'green')
+  $('#signOut-message').toggle()
+  $('div').toggle()
+  $('table').toggle()
 }
 
 const signOutFailure = function (error) {
-  $('#signOut-message').text('Password Change Unsuccessful')
+  $('#signOut-message').text('Already Signed Out')
   $('#signOut-message').css('background-color', 'red')
   console.log(error)
 }
